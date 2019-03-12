b3a75f2f6dbc5d970c096d378289e65daef98984
Grant Joy <grant.joy@distilnetworks.com>
2019-03-12 16:55:10 -0700

    adjusting docker script

--- debian/docker_package.sh
+++ debian/docker_package.sh
@@ -4,7 +4,7 @@ set -aeux
 
 # This should be unique to your repository. Most have a README documenting how to build and produce a binary or
 # other type of artifact. In this case the docker_build.sh script contains npm commands.
-debian/docker-build.sh "./build"
+debian/docker-build.sh "script/build"
 
 # Build the image that will run debuild.
 docker build . -f Dockerfile -t debuild
